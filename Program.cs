8f25c20faca892a733be7b048460315847dcfe9e
Matheus Ferreira <matheus.sp929305@gmail.com>
2023-10-16 23:14:31 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+
+void exibirMensagemDeBoasVindas (){
+
+    Console.WriteLine(@"
+░██████╗██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(mensagemDeBoasVindas);
+
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("Digite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as banda");
+    Console.WriteLine("Digite 3 para para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a media de uma banda");
+    Console.WriteLine("Digite -1 para registrar uma banda");
+
+    Console.Write("\nDigite a sua opção: ");
+    // Console.ReadLine();
+    string opcaoEscolhida = Console.ReadLine()!;
+    if(opcaoEscolhida == 1)
+    {
+        Console.WriteLine("")
+    }
+}
+exibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
